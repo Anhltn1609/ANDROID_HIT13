c1af2af0f5d7f8b132b7eb15aa8281148499c193
Anhltn1609 <Anhsltn1234r@gmail.com>
2023-04-01 23:21:51 +0700

    lan1

--- app/src/main/java/com/example/b3/MainActivity.kt
+++ app/src/main/java/com/example/b3/MainActivity.kt
@@ -1,54 +1,142 @@
 package com.example.b3
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.widget.Button
 
 import android.widget.TextView
 
+@Suppress("UNUSED_EXPRESSION")
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val txt: TextView = findViewById(R.id.text_view_title)
-//        val tt: TextView = findViewById(R.id.text_view_number)
-//        val button: Button = findViewById(R.id.nhan)
-//        var cnt: Int = 0;
-//
-//
-//
-//        Log.e("text", txt.text.toString())
-//
-//        txt.setOnClickListener{
-//            txt.text = "World"
-//            txt.setTextColor(resources.getColor(R.color.purple_200))
-//            txt.setTextSize(200F)
-//        }
-//        button.setOnClickListener {
-//            tt.setText(cnt++.toString())
-//            Log.e("a", tt.text.toString())
-//        }
+        val display: TextView = findViewById(R.id.display)
+        val Symbol: String? = null
+        val ac: TextView = findViewById(R.id.AC)
+        val plus_or_minus: TextView = findViewById(R.id.plus_or_minus)
+        val percent: TextView = findViewById(R.id.percent)
+        val division: TextView = findViewById(R.id.division)
+        val one: TextView = findViewById(R.id.one)
+        val two: TextView = findViewById(R.id.two)
+        val three: TextView = findViewById(R.id.three)
+        val four: TextView = findViewById(R.id.four)
+        val five: TextView = findViewById(R.id.five)
+        val six: TextView = findViewById(R.id.six)
+        val seven: TextView = findViewById(R.id.seven)
+        val eight: TextView = findViewById(R.id.eight)
+        val nine: TextView = findViewById(R.id.nine)
+        val multi: TextView = findViewById(R.id.multi)
+        val minus: TextView = findViewById(R.id.minus)
+        val plus: TextView = findViewById(R.id.plus)
+        val zero: TextView = findViewById(R.id.zero)
+        val comma: TextView = findViewById(R.id.comma)
 
-        val b1: Button = findViewById(R.id.b1)
-        val b2: Button = findViewById(R.id.b2)
-        val b3: Button = findViewById(R.id.b3)
-        val b4: Button = findViewById(R.id.b4)
+        ac.setOnClickListener { display.text = '0'.toString() }
 
-        b2.setOnClickListener{
-            b1.background = b2.background
-            b1.text = b2.text
+        one.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "1"
+//            else if(values.text.toString() )
+            else display.text = display.text.toString() + "1"
+        }
+        two.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "2"
+            else display.text = display.text.toString() + "2"
+        }
+        three.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "3"
+            else display.text = display.text.toString() + "3"
+        }
+            four.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "4"
+            else display.text = display.text.toString() + "4"
+        }
+        five.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "5"
+            else display.text = display.text.toString() + "5"
+        }
+        six.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "6"
+            else display.text = display.text.toString() + "6"
+        }
+        seven.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "7"
+            else display.text = display.text.toString() + "7"
+        }
+        eight.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "8"
+            else display.text = display.text.toString() + "8"
+        }
+        nine.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "9"
+            else display.text = display.text.toString() + "9"
+        }
+        zero.setOnClickListener {
+            if(display.text == '0'.toString()) display.text = "0"
+            else display.text = display.text.toString() + "0"
         }
 
-        b3.setOnClickListener{
-            b1.setBackgroundColor(Color.BLUE)
-            b1.text = b3.text
+        fun TextView.OnClick(click: (values: String) -> Unit) {
+            this.setOnTouchListener { _, motionEvent ->
+                when (motionEvent.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        click(this.text.toString())
+                        this.setBackgroundResource(R.drawable.border2)
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        this.setBackgroundResource(R.drawable.border)
+                    }
+                }
+                true
+            }
         }
 
-        b4.setOnClickListener{
-            b1.background = b4.background
-            b1.text = b4.text
+        fun number() {
+            zero.OnClick { values -> "0" }
+
+            one.OnClick { values -> "1" }
+
+            two.OnClick { values -> "2" }
+
+            three.OnClick { values -> "3" }
+
+            four.OnClick { values -> "4" }
+
+            five.OnClick { values -> "5" }
+
+            six.OnClick { values -> "6" }
+
+            seven.OnClick { values -> "7" }
+
+            eight.OnClick { values -> "8" }
+
+            nine.OnClick { values -> "9" }
         }
+
+
+        fun symbol() {
+            plus.OnClick { values -> "+" }
+
+            minus.OnClick { values -> "-" }
+
+            multi.OnClick { values -> "*" }
+
+            division.OnClick { values -> " / " }
+
+            percent.OnClick { values -> " % " }
+
+            comma.OnClick { values -> " . " }
+
+
+        }
+
+
     }
+
+
 }
